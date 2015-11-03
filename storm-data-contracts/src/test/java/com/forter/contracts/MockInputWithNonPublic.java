22af642b2227c3cc22a73fb49c9a77f5e2db6c8c
Nadav Amir <nadav@forter.com>
2015-11-03 18:57:22 +0200

    Added handling for non public fields

---
+++ storm-data-contracts/src/test/java/com/forter/contracts/MockInputWithNonPublic.java
@@ -0,0 +1,24 @@
+package com.forter.contracts;
+
+import com.google.common.base.Optional;
+import org.hibernate.validator.valuehandling.UnwrapValidatedValue;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
+public class MockInputWithNonPublic {
+
+    @NotNull
+    @DecimalMin(value="0", inclusive=true)
+    public Integer x;
+
+    @UnwrapValidatedValue
+    @DecimalMin(value="1", inclusive=true)
+    public Optional<Integer> y;
+
+    @NotNull
+    Integer z;
+
+    @UnwrapValidatedValue
+    Optional<String> a;
+}
