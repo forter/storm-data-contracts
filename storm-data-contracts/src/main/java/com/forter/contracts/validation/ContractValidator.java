0cdf2a692f7adc87be399a2c356c7ca11e0500d0
Re'em Bensimhon <reem@forter.com>
2019-05-02 16:09:51 +0300

    More fixes for JDK12

--- storm-data-contracts/src/main/java/com/forter/contracts/validation/ContractValidator.java
+++ storm-data-contracts/src/main/java/com/forter/contracts/validation/ContractValidator.java
@@ -5,6 +5,7 @@ import javax.validation.Validation;
 import javax.validation.ValidationException;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+
 import org.hibernate.validator.HibernateValidator;
 import org.hibernate.validator.internal.util.TypeResolutionHelper;
 
